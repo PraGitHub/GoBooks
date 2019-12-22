1ec2a1d5b8e8ca0123404943c2f72055f8a4643f
prashanth <prashanthhn2509@gmail.com>
2019-12-22 12:38:47 +0530

    improved installDep.sh

--- installDep.sh
+++ installDep.sh
@@ -1,10 +1,20 @@
-go get github.com/GoIncremental/negroni-sessions/cookiestore
-go get github.com/goincremental/negroni-sessions
-go get github.com/gorilla/mux
-go get github.com/lib/pq
-go get github.com/mattn/go-sqlite3
-go get github.com/urfave/negroni
-go get github.com/yosssi/ace
-go get golang.org/x/crypto/bcrypt
-go get gopkg.in/gorp.v1
-go get github.com/tools/godep
+log () {
+    echo "`date ` $1"
+}
+
+execute () {
+    log "Executing $1"
+    $1
+    log "Done executing $1"
+}
+
+execute "go get github.com/GoIncremental/negroni-sessions/cookiestore"
+execute "go get github.com/goincremental/negroni-sessions"
+execute "go get github.com/gorilla/mux"
+execute "go get github.com/lib/pq"
+execute "go get github.com/mattn/go-sqlite3"
+execute "go get github.com/urfave/negroni"
+execute "go get github.com/yosssi/ace"
+execute "go get golang.org/x/crypto/bcrypt"
+execute "go get gopkg.in/gorp.v1"
+execute "go get github.com/tools/godep"
